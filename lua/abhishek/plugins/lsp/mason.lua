84f7956fb694ea8f1d4dc2c1fe274d7387956997
Abhishek Mishra <abhiusedu@gmail.com>
2023-03-07 21:16:45 -0800

    Fix some issues with luals which was not working for MacOS.

--- lua/abhishek/plugins/lsp/mason.lua
+++ lua/abhishek/plugins/lsp/mason.lua
@@ -16,7 +16,7 @@ mason_lspconfig.setup({
         "html",
         "cssls",
         "tailwindcss",
-        "sumneko_lua",
+        "lua_ls",
         "jdtls",
         "groovyls",
         "quick_lint_js",
